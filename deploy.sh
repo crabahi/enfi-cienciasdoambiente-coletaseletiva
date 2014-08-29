dfcca0b0b6dc3fba78cc0b869ee92498bfe95d0c
Rafael Santos <rafael.santos.193@gmail.com>
2014-08-29 19:46:23 -0300

    Estrutura inicial

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+# Se já existir arquivo app_bkp_maitenance.php, pula esta etapa
+if [ -f web/app_bkp_maintenance.php ]; then
+    echo '*** "Modo de manutenção" já estava ativado'
+else
+    # Se não existir arquivo web/maintenance.php, o comando copy daria erro, então pula
+    if [ -f web/maintenance.php ] && [ -f web/app.php ]; then
+        echo '*** enabling "Modo de manutenção"' && \
+        mv web/app.php web/app_bkp_maintenance.php && \
+        cp web/maintenance.php web/app.php
+    else
+        echo '*** "Modo de manutenção" não pode ser ativado (maintenance.php ou app.php não existe)'
+    fi
+fi
+
+# A partir de agora, tem que tudo dar certo para sair do modo de manutenção
+echo '*** updating git repo' && \
+git pull && \
+echo '*** composer install' && \
+php composer.phar install && \
+echo '*** symbolic linking web/components' && \
+ln -f -s -n ../components web/components && \
+echo '*** clearing prod cache' && \
+php app/console cache:clear --env=prod && \
+echo '*** assetic dump' && \
+php app/console assetic:dump --env=prod && \
+echo '*** assets install' && \
+php app/console assets:install web --symlink --relative && \
+echo '*** migrações que devem ser feitas no banco de dados' && \
+sf doctrine:schema:update --dump-sql && \
+echo '*** disabling "Modo de manutenção"' && \
+mv web/app_bkp_maintenance.php web/app.php && \
+echo '*** success!'
---
+++ src/Enfi/CienciasDoAmbiente/SiteBundle/EnfiCienciasDoAmbienteSiteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Enfi\CienciasDoAmbiente\SiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EnfiCienciasDoAmbienteSiteBundle extends Bundle
+{
+}
