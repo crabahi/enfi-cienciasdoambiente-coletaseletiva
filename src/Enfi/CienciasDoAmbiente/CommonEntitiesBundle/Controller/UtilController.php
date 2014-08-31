664dc82bfe47e23216a166f43eb50f1ac334d8ee
Rafael Santos <rafael.santos.193@gmail.com>
2014-08-31 20:58:32 -0300

    Finder para pegar versão do commit

--- src/Enfi/CienciasDoAmbiente/CommonEntitiesBundle/Controller/UtilController.php
+++ src/Enfi/CienciasDoAmbiente/CommonEntitiesBundle/Controller/UtilController.php
@@ -5,6 +5,7 @@ namespace Enfi\CienciasDoAmbiente\CommonEntitiesBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Finder\Finder;
 
 class UtilController extends Controller
 {
@@ -13,7 +14,11 @@ class UtilController extends Controller
      */
     public function gitVersionAction()
     {
-        $git_version = 'git-test-hash';
+        $finder = new Finder();
+        $finder->files()->name('git-current-commit.txt');
+        foreach ($finder as $file) {
+                $git_version = $file->getContents();
+        }
         return array('git_version' => $git_version);
     }
 }
