2d0df052c04377fa35cbcf02f47ca36f38f6715f
Rafael Santos <rafael.santos.193@gmail.com>
2014-08-31 00:16:48 -0300

    Adicionado itens falsos no mapa, para protótipo

---
+++ web/phpsqlajax_genxml.php
@@ -0,0 +1,4 @@
+<markers>
+<marker name="Lixeira do Lago" address="Próx. ao lago da UFSCar" lat="-21.985" lng="-47.881" type="restaurant" />
+<marker name="Descarte de Lixo Eletrônico" address="Entrada da Biblioteca Comunitária" lat="-21.983" lng="-47.881" type="bar" />
+</markers>
